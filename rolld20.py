7d64b0a52eed533f2975fd562796d9a1ada5b5af
Lucas Vinn <90799507+LucasVinn@users.noreply.github.com>
2022-10-28 18:24:24 -0300

    Add files via upload

---
+++ rolld20.py
@@ -0,0 +1,15 @@
+import random
+
+answer = str(input('Rolar o D20?'))
+
+if answer == 'Sim':
+    numberandom = random.randint(1,20)
+    print(numberandom)
+    
+else:
+    print('Comando finalizado')
+    
+    
+
+
+
